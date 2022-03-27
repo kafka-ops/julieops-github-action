b8dcbb75264c2c7e872b61777e49b90cad4e1ed3
Pere Urbon <pere@confluent.io>
2022-03-27 12:15:26 +0200

    first simple structure provided in the action

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
-# Container image that runs your code
-FROM alpine:3.10
+# Build out of the provided JulieOps docker image
+FROM purbon/kafka-topology-builder:4.1.1
 
-# Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
-
-# Code file to execute when the docker container starts up (`entrypoint.sh`)
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,25 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+##
+# Running a JulisOps Job for $1
+####
+#    --brokers <arg>                  The Apache Kafka server(s) to connect
+#                                     to.
+#    --clientConfig <arg>             The client configuration file.
+#    --dryRun                         Print the execution plan without
+#                                     altering anything.
+#    --help                           Prints usage information.
+#    --overridingClientConfig <arg>   The overriding AdminClient
+#                                     configuration file.
+#    --plans <arg>                    File describing the predefined plans
+#    --quiet                          Print minimum status update
+#    --topology <arg>                 Topology config file.
+#    --validate                       Only run configured validations in
+#                                     your topology
+#    --version                        Prints useful version information.
+###
+
+julie-ops-cli.sh  \
+    --brokers $1 \
+    --topology $2 \
+    --clientConfig $3
